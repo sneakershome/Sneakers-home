b3ff5756a86f6283bf4fda9bdbabcbc660289fde
sneakershome <134550934+sneakershome@users.noreply.github.com>
2023-05-24 14:03:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function myFunction() {
+  var txt;
+  if (confirm("you are about to login or create a session")) {
+    txt = "You pressed OK!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+function Function() {
+  var txt;
+  if (confirm("you are about to navegate out of the website")) {
+    txt = "You pressed OK!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
